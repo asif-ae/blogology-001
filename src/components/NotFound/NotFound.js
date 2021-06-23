df5b41c0419d2374b3d229b4b9b4c56daf08d8db
Asif Estiak <me.asif.ae@gmail.com>
2021-06-23 20:20:25 +0600

    not found route added

--- src/components/NotFound/NotFound.js
+++ src/components/NotFound/NotFound.js
@@ -4,9 +4,11 @@ import { Fragment } from 'react';
 const NotFound = () => {
   return (
     <Fragment>
-      <Box display="flex" justifyContent="center" textAlign="center" flexDirection="column">
-        <Box fontSize="h1.fontSize" m={1}>404</Box>
-        <Box Box fontSize="h3.fontSize" m={1}>Page Not Found</Box>
+      <Box display="flex" justifyContent="center" alignItems="center" className="height-100">
+        <Box textAlign="center" color="error.dark">
+          <Box fontSize="h3.fontSize" m={1}>404</Box>
+          <Box Box fontSize="h4.fontSize" m={1}>Page Not Found!</Box>
+        </Box>
       </Box>
     </Fragment>
   );
